eec12407239a28d88730f51185eedb2d17df41cd
RachelCohencd <79844243+RachelCohencd@users.noreply.github.com>
2021-08-31 22:54:44 +0300

    Update Grid.js

--- src/Grid.js
+++ src/Grid.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
+
 const Grid = ({ config, data }) => (
   <table>
     <thead>
     <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
+     {config.map(c=><th>{c.title}</th>)}
     </tr>
     </thead>
     <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
+    {data.map(item=>{return(<tr>{config.map(col=>{return<td>{item[col.field]}</td>})}</tr>)})}
     </tbody>
   </table>
 );
